de190f01a74f6162b0fc20aeff076c69cf06c7ea
bhvsh <75534159+bhvsh@users.noreply.github.com>
2023-07-26 18:38:06 +0530

    Implemented block status for qurbat's list

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ async function fetchUrl() {
         let url = tabs[0].url;
         let domain = url.split('/')[2];
         document.getElementById("currentDomain").innerHTML=domain;
+
+        fetch('https://raw.githubusercontent.com/qurbat/blocked-hosts/main/compiled_block_list.txt')
+        .then(response => response.text())
+        .then(text => text.split(/\r?\n/))
+        .then(text => 
+            {
+                if (text.includes(domain)) {
+                    document.getElementById("blockStatus").innerHTML="Blocked";
+                }
+                else {
+                    document.getElementById("blockStatus").innerHTML="Not Blocked";
+                }
+            })
+        .catch(err => console.log('Error: ', err));
+
     });
 }
 fetchUrl();
