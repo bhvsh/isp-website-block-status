21e141caf7c8d2d9d849cbecdeef1f18e25ff9e1
bhvsh <75534159+bhvsh@users.noreply.github.com>
2023-08-15 21:00:17 +0530

    Added support for Adaptive Dark Mode

--- script.js
+++ script.js
@@ -11,14 +11,19 @@ async function fetchUrl() {
             {
                 if (text.includes(domain) || text.includes(domain.replace('www.',''))) {
                     document.getElementById("blockStatusACT").innerHTML="Blocked";
-                    document.getElementById("blockStatusACT").style.color = "red";
+                    document.getElementById("blockStatusACT").className = "red";
                 }
                 else {
                     document.getElementById("blockStatusACT").innerHTML="Not Blocked";
-                    document.getElementById("blockStatusACT").style.color = "green";
+                    document.getElementById("blockStatusACT").className = "green";
                 }
             })
-        .catch(err => console.log('Error: ', err));
+            .catch(err => {
+                console.log('Error: ', err);
+                document.getElementById("blockStatusACT").innerHTML="Could not load list";
+                document.getElementById("blockStatusACT").className = "red";
+                document.getElementById("repoBadgeACT").innerHTML = "Open list";
+            });
 
         fetch('https://raw.githubusercontent.com/captn3m0/airtel-blocked-hosts/airtel-fiber/airtel-fiber-blocked-hosts.txt')
         .then(response => response.text())
@@ -27,14 +32,19 @@ async function fetchUrl() {
             {
                 if (text.includes(domain) || text.includes(domain.replace('www.',''))) {
                     document.getElementById("blockStatusAirtel").innerHTML="Blocked";
-                    document.getElementById("blockStatusAirtel").style.color = "red";
+                    document.getElementById("blockStatusAirtel").className = "red";
                 }
                 else {
                     document.getElementById("blockStatusAirtel").innerHTML="Not Blocked";
-                    document.getElementById("blockStatusAirtel").style.color = "green";
+                    document.getElementById("blockStatusAirtel").className = "green";
                 }
             })
-        .catch(err => console.log('Error: ', err));
+        .catch(err => {
+            console.log('Error: ', err);
+            document.getElementById("blockStatusAirtel").innerHTML="Could not load list";
+            document.getElementById("blockStatusAirtel").className = "red";
+            document.getElementById("repoBadgeAirtel").innerHTML = "Open list";
+        });
 
     });
 }
